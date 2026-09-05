e87d1d86c3e3f7844fa9140bb49c60ee5b600053
Ebrahassad <Ebrahassadi77@gmail.com>
2026-09-05 13:49:33 +0200

    Remove custom release signing completely

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,15 +6,6 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-val keystorePropertiesFile = rootProject.file("key.properties")
-val keystoreProperties = Properties()
-
-if (!keystorePropertiesFile.exists()) {
-    throw GradleException("android/key.properties not found")
-}
-
-keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-
 android {
     namespace = "hassadi.neonbreaker.pro"
     compileSdk = flutter.compileSdkVersion
@@ -31,30 +22,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-    }
-
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties.getProperty("keyAlias")
-                ?: throw GradleException("keyAlias missing")
-
-            keyPassword = keystoreProperties.getProperty("keyPassword")
-                ?: throw GradleException("keyPassword missing")
-
-            storePassword = keystoreProperties.getProperty("storePassword")
-                ?: throw GradleException("storePassword missing")
-
-            storeFile = rootProject.file(
-                keystoreProperties.getProperty("storeFile")
-                    ?: throw GradleException("storeFile missing")
-            )
-        }
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("release")
-        }
     }
 }
 
